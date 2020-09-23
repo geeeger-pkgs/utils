b5f703512207079bcc1fe718c76cc102f47ede05
geeeger <lph1988520@sina.com>
2020-09-23 15:23:57 +0800

    fix(structure): fix spelling

--- packages/structure/src/index.ts
+++ packages/structure/src/index.ts
@@ -1,5 +1,5 @@
 export { default as BinaryTree } from './binarytree';
 export { default as HashMap } from './hashmap';
-export { default as LinkedList } from './LinkedList';
+export { default as LinkedList } from './linkedlist';
 export { default as Queue } from './queue';
 export { default as Stack } from './stack';
